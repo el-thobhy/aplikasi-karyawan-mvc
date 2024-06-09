b0935fdf6a3126bfcfbcc209978ab727530f41a7
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-06-09 21:25:13 +0700

    first init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aplikasi_karyawan_fe_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
